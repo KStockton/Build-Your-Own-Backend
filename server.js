920ece0751dbaea2dafd42f27f126085b0b5e3ab
Michael KS <kingstockton@gmail.com>
2019-06-26 21:25:34 -0600

    add endpoint for player id

--- server.js
+++ server.js
@@ -18,6 +18,21 @@ app.get('/api/v1/teams', (request, response) => {
     })
 });
 
+app.get('/api/v1/players/:id',(request, response) => {
+  const { id } = request.params
+  console.log(id, 'id')
+  database('players').select()
+    .then((players) => {
+      let findPlayer = players.find(player => player.id == id)
+      if(!findPlayer) response.status(404).json(`Player not found with ${id}`)
+      response.status(200).json(findPlayer)
+    })
+    .catch((error) => {
+      response.status(500).json({error})
+    })
+});
+
+
 
 
 
